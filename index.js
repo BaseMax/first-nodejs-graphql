2b30ae117239f04e570295aabe712b7d497d2f94
Max Base <MaxBaseCode@Gmail.Com>
2020-12-24 04:07:51 +0330

    Adding RootQueryType, schema, BookType

--- index.js
+++ index.js
@@ -28,16 +28,30 @@ const books = [
 	{id: 8, name: 'Beyond the Shadows', authorId: 3},
 ]
 
-const schema = new GraphQLSchema({
-	query: new GraphQLObjectType({
-		name: 'HelloWorld',
-		fields: () => ({
-			message: {
-				type: GraphQLString,
-				resolve: (parent, args) => 'Hello World'
-			}
-		})
+const BookType = new GraphQLObjectType({
+	name: 'Book',
+	description: 'This represents a book written by an author',
+	fields: () => ({
+		id: {type: GraphQLNonNull(GraphQLInt)},
+		name: {type: GraphQLNonNull(GraphQLString)},
+		authorId: {type: GraphQLNonNull(GraphQLInt)},
 	})
+})
+
+const RootQueryType = new GraphQLObjectType({
+	name: 'Query',
+	description: 'Root Query',
+	fields: () => ({
+		books: {
+			type: new GraphQLList(BookType),
+			description: 'List of All Books',
+			resolve: () => books
+		},
+	})
+})
+
+const schema = new GraphQLSchema({
+	query: RootQueryType
 })
 
 app.use('/graphql', graphqlHTTP({
