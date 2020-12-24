03744e1cf7ed93a40832a9b4ba599df357d78a52
Max Base <MaxBaseCode@Gmail.Com>
2020-12-24 04:08:10 +0330

    Adding AuthorType

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ const books = [
 	{id: 8, name: 'Beyond the Shadows', authorId: 3},
 ]
 
+const AuthorType = new GraphQLObjectType({
+	name: 'Author',
+	description: 'This represents a author of a book',
+	fields: () => ({
+		id: {type: GraphQLNonNull(GraphQLInt)},
+		name: {type: GraphQLNonNull(GraphQLString)},
+	})
+})
+
 const BookType = new GraphQLObjectType({
 	name: 'Book',
 	description: 'This represents a book written by an author',
