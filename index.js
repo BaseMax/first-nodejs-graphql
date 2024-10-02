6ef019b3813ed11c72fb9144317c38cff70e4ef3
MaxBased <hoseintahan84@gmail.com>
2024-10-03 00:41:49 +0330

    Add  mongodb support to index.js

--- index.js
+++ index.js
@@ -4,68 +4,62 @@
  * @Repository: https://github.com/BaseMax/first-nodejs-graphql
  * @Date: 2020-12-24
  */
+const mongoose = require('mongoose');
+const Author = require('./models/author');
+const Book = require('./models/book');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
 const {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
   GraphQLList,
-  GraphQLInt,
   GraphQLNonNull
-} = require('graphql')
+} = require('graphql');
 
-const app = express()
-const PORT = 5000
 
-const authors = [
-  {id: 1, name: 'J. K. Rowling'},
-  {id: 2, name: 'J. R. R. Tolkien'},
-  {id: 3, name: 'Brent Weeks'},
-]
+const app = express();
+const PORT = 5000;
 
-const books = [
-  {id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1},
-  {id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1},
-  {id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1},
-  {id: 4, name: 'The Fellowship of the Ring', authorId: 2},
-  {id: 5, name: 'The Two Towers', authorId: 2},
-  {id: 6, name: 'The Return of the King', authorId: 2},
-  {id: 7, name: 'The Way of Shadows', authorId: 3},
-  {id: 8, name: 'Beyond the Shadows', authorId: 3},
-]
+
+mongoose.connect('mongodb://127.0.0.1:27017/library_db')
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 
 const AuthorType = new GraphQLObjectType({
   name: 'Author',
-  description: 'This represents a author of a book',
+  description: 'This represents an author of a book',
   fields: () => ({
-    id: {type: GraphQLNonNull(GraphQLInt)},
-    name: {type: GraphQLNonNull(GraphQLString)},
+    id: { type: GraphQLNonNull(GraphQLString) },
+    name: { type: GraphQLNonNull(GraphQLString) },
     books: {
       type: new GraphQLList(BookType),
-      resolve: (author) => {
-        return books.filter(book => book.authorId === author.id)
+      resolve: async (author) => {
+        return await Book.find({ authorId: author.id });
       }
     }
   })
-})
+});
+
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   description: 'This represents a book written by an author',
   fields: () => ({
-    id: {type: GraphQLNonNull(GraphQLInt)},
-    name: {type: GraphQLNonNull(GraphQLString)},
-    authorId: {type: GraphQLNonNull(GraphQLInt)},
+    id: { type: GraphQLNonNull(GraphQLString) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLString) },
     author: {
       type: AuthorType,
-      resolve: (book) => {
-        return authors.find(author => author.id === book.authorId)
+      resolve: async (book) => {
+        return await Author.findById(book.authorId);
       }
     }
   })
-})
+});
+
 
 const RootQueryType = new GraphQLObjectType({
   name: 'Query',
@@ -75,30 +69,31 @@ const RootQueryType = new GraphQLObjectType({
       type: BookType,
       description: 'A Single Book',
       args: {
-        id: { type: GraphQLInt }
+        id: { type: GraphQLString }
       },
-      resolve: (parent, args) => books.find(book => book.id === args.id)
+      resolve: async (parent, args) => await Book.findById(args.id),
     },
     books: {
       type: new GraphQLList(BookType),
       description: 'List of All Books',
-      resolve: () => books
+      resolve: async () => await Book.find(),
     },
     author: {
       type: AuthorType,
       description: 'A Single Author',
       args: {
-        id: {type: GraphQLInt }
+        id: { type: GraphQLString }
       },
-      resolve: () => authors.find(author => author.id === args.id)
+      resolve: async (parent, args) => await Author.findById(args.id),
     },
     authors: {
       type: new GraphQLList(AuthorType),
       description: 'List of All Authors',
-      resolve: () => authors
+      resolve: async () => await Author.find(),
     }
   })
-})
+});
+
 
 const RootMutationType = new GraphQLObjectType({
   name: 'Mutation',
@@ -109,12 +104,12 @@ const RootMutationType = new GraphQLObjectType({
       description: 'Add a book',
       args: {
         name: { type: GraphQLNonNull(GraphQLString) },
-        authorId: { type: GraphQLNonNull(GraphQLInt) },
+        authorId: { type: GraphQLNonNull(GraphQLString) },
       },
-      resolve: (parent, args) => {
-        const book = { id: books.length + 1, name: args.name, authorId: args.authorId}
-        books.push(book)
-        return book
+      resolve: async (parent, args) => {
+        const book = new Book({ name: args.name, authorId: args.authorId });
+        await book.save();
+        return book;
       }
     },
     addAuthor: {
@@ -123,49 +118,28 @@ const RootMutationType = new GraphQLObjectType({
       args: {
         name: { type: GraphQLNonNull(GraphQLString) },
       },
-      resolve: (parent, args) => {
-        const author = { id: authors.length + 1, name: args.name}
-        authors.push(author)
-        return author
+      resolve: async (parent, args) => {
+        const author = new Author({ name: args.name });
+        await author.save();
+        return author;
       }
     }
   })
-})
+});
+
 
 const schema = new GraphQLSchema({
   query: RootQueryType,
   mutation: RootMutationType,
-})
+});
 
-// const schema = new GraphQLSchema({
-//  query: new GraphQLObjectType({
-//    name: 'HelloWorld',
-//    fields: () => ({
-//      message: {
-//        type: GraphQLString,
-//        resolve: (parent, args) => 'Hello World'
-//      }
-//    })
-//  })
-// })
 
-// app.use(bodyParser.json())
-
-// app.use('/graphql', graphqlHTTP({
-//   schema: schema,
-//   graphiql: true
-// }))
-
-app.get('/graphql', graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true
-}))
+}));
 
-app.post('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: false
-}))
 
 app.listen(PORT, () => {
-  console.log('Server Running...')
-})
+  console.log(`Server running on port ${PORT}`);
+});
